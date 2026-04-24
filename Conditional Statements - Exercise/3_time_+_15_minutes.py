d177fe629b0ab39cb1a129453acd3ef793c49715
arrisingme <arrisingme@gmail.com>
2026-04-24 11:54:44 +0300

    Update 3_time_+_15_minutes.py

--- Conditional Statements - Exercise/3_time_+_15_minutes.py
+++ Conditional Statements - Exercise/3_time_+_15_minutes.py
@@ -1,15 +1,16 @@
-number = int(input())
+hour = int(input())
+minutes = int(input())
 
-if number <= 100:
-    bonus = 5
-elif 100 < number <= 1000:
-    bonus = (number * 0.20)
+new_minutes = minutes + 15
+
+if new_minutes > 59:
+    hour += 1
+    new_minutes -= 60
+
+if hour > 23:
+    hour = 0
+
+if new_minutes < 10:
+    print(f"{hour}:0{new_minutes}")
 else:
-    bonus = (number * 0.10)
-if number % 2 == 0:
-    bonus += 1
-if number % 10 == 5:
-    bonus += 2
-
-print(bonus)
-print(number + bonus)
+    print(f"{hour}:{new_minutes}")
