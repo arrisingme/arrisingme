46238eba01525aef04a21d4b7465f09c3aadd0e4
arrisingme <arrisingme@gmail.com>
2026-04-30 09:39:07 +0300

    Add files via upload

---
+++ Conditional Statements Advanced - Lab/1_day_of_the_week.py
@@ -0,0 +1,21 @@
+number = int(input())
+result = ""
+
+if number == 1:
+    result = "Monday"
+elif number == 2:
+    result = "Tuesday"
+elif number == 3:
+    result = "Wednesday"
+elif number == 4:
+    result = "Thursday"
+elif number == 5:
+    result = "Friday"
+elif number == 6:
+    result = "Saturday"
+elif number == 7:
+    result = "Sunday"
+else:
+    result = "Error"
+
+print(result)
