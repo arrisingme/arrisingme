7c39e46646339bb5c0f85aa52c30001b26f79ed4
arrisingme <arrisingme@gmail.com>
2026-05-22 14:00:25 +0300

    Add files via upload

---
+++ For Loop - Lab/10_odd_even_sum.py
@@ -0,0 +1,18 @@
+n = int(input())
+even_sum = 0
+odd_sum = 0
+
+for i in range(n):
+    new_number = int(input())
+
+    if i % 2 == 0:
+        even_sum += new_number
+    else:
+        odd_sum += new_number
+
+if even_sum == odd_sum:
+    print("Yes")
+    print(f"Sum = {even_sum}")
+else:
+    print("No")
+    print(f"Diff = {abs(even_sum - odd_sum)}")
